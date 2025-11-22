3055a9748596183997a1a93a8f236a56235bb413
backup-heavenly-demons <sarifpede@gmail.com>
2025-11-22 09:50:17 +0700

    Update console.py

--- app/console.py
+++ app/console.py
@@ -86,14 +86,15 @@ def clear():
 def print_ascii_art():
     art = r"""
  [neon_cyan]
-          .                                                    .
-        .n                   .       .      .                  n.
-  .   .dP                  dP       art      9b                 9b.    .
- 4    qXb         .       dX     kang-ukon    Xb       .        dXp     t
-dX.    9Xb      .dXb    .dxb        dev        dXb.    dXP     dx6      .Xb
-9XXb._       _.dXXXXb dXXXXbo.       '        .odXXXXb dXXXXb._       _.dXXP
- 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
-  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+                                      .
+          .                          /:\                         .
+        .n                   .      //!\\     .                  n.
+  .   .dP                  dP     ///art\\\    9b                 9b.    .
+ 4    qXb         .       dX     [kang-ukon]    Xb        .       dXp     t
+dX.    9Xb      .dXb    .dxb      \\\dev///      dXb.    dXP     dx6      .Xb
+9XXb._       _.dXXXXb dXXXXbo.      \\¡//      .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.    \:/     .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b ' d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
     `9XXXXXXXXXXXP' `9XX' HEAVENLY `98v8P' HEAVENLY `XXP' `9XXXXXXXXXXXP'
         ~~~~~~~       9X.  DEMONS  .db|db.  DEMONS  .XP       ~~~~~~~
                         )b.      .dP'`v'`9b.     .dX(
